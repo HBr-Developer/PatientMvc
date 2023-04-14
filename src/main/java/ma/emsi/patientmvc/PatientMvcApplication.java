4000735fa844424d07be2fb6dd0f200538d98ec6
HBR-Developer <hananeelbarbari2000@gmail.com>
2023-04-14 23:47:04 +0000

    Second part patient mvc

--- src/main/java/ma/emsi/patientmvc/PatientMvcApplication.java
+++ src/main/java/ma/emsi/patientmvc/PatientMvcApplication.java
@@ -17,13 +17,13 @@ public class PatientMvcApplication {
     public static void main(String[] args) {
         SpringApplication.run(PatientMvcApplication.class, args);
     }
-    @Bean //Pour que ça s'execute au démarrage
+ //   @Bean //Pour que ça s'execute au démarrage
     CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
         return args -> {
-            patientRepository.save(new Patient(null, "Ikram", new Date(), false, 12));
-            patientRepository.save(new Patient(null, "Hanane", new Date(), true, 20));
-            patientRepository.save(new Patient(null, "Zaki", new Date(), false, 50));
-            patientRepository.save(new Patient(null, "Rayane", new Date(), true, 90));
+            patientRepository.save(new Patient(null, "Ikram", new Date(), false, 112));
+            patientRepository.save(new Patient(null, "Hanane", new Date(), true, 120));
+            patientRepository.save(new Patient(null, "Zaki", new Date(), false, 510));
+            patientRepository.save(new Patient(null, "Rayane", new Date(), true, 190));
             patientRepository.findAll().forEach(
                     p -> {
                         System.out.println(p.getNom());
